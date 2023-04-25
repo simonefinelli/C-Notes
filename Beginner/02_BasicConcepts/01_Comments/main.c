559ac5025ee2d38e8747d130b1498fd621805c7e
Simone Finelli <simonefinelli.work@gmail.com>
2023-04-25 13:51:54 +0200

    feat: add comments in BasicConcepts

---
+++ Beginner/02_BasicConcepts/01_Comments/main.c
@@ -0,0 +1,15 @@
+/*
+ * Name: main.c
+ * Purpose: Print the famous string.
+ * Date: 1970/01/01
+ * Author: Simox
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    // print the statement Hello World
+    printf("Hello, World!\n");
+    return 0;
+}
